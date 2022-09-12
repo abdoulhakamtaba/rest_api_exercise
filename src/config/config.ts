b32ea6b5509b7088b745b5d62237aae2fa7f0720
abdoulhakamtaba <pymailing123@gmail.com>
2022-09-12 14:31:36 +0100

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "tabaudacitydev",
+    "password": "tabaudacitydev",
+    "database": "tabaudacitydev",
+    "host": "tabaudacitydev.cqvkikji8hlv.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "tabaudacitydev-s3"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
